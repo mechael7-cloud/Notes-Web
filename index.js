2adf447acb9c470cb92052edc9dbf3dafa6e8217
mechael7 <mechaelmandausanduo7@gmail.com>
2026-03-22 22:50:29 +0700

    Menbuat function untuk mengambil data dari JSON ke html melalui Javascript

--- index.js
+++ index.js
@@ -8,92 +8,25 @@ const removeNote = document.getElementById("remove-note");
 const inputJudul = document.getElementById("inputJudul");
 const inputContent = document.getElementById("inputContent");
 const buttonSave = document.getElementById("save-button");
+const noteContainer = document.getElementById("main-container")
 
 
-function keepNotes(e) {
-    event.preventDefault()
+buttonSave.addEventListener("click", () => {
+    cards.classList.remove("show");
+    headerPage.classList.remove("tampil")
+})
 
-    let keepJudul = inputJudul.value.trim();
-    let keepContent = inputContent.value.trim();
+buttonAdd.addEventListener("click", function () {
+    cards.classList.toggle("show");
+    headerPage.classList.toggle("tampil");
+    noteContainer.classList.toggle("lihat");
+});
 
-    if (editNote) {
-        const keepNote = notes.findIndex(note => note.id == editNote)
-        notes[keepNote] = {
-        ...notes[keepNote],
-            keepJudul: keepJudul,
-            keepContent: keepContent,
-        }
-    } else {
-        notes.unshift({
-            id: generateId(),
-            Judul: keepJudul,
-            Content: keepContent
-        })
-    }
+removeNote.addEventListener("click", () => {
 
-     saveNote();
-}
-
-
-function loadingNotes () {
-    const simNotes = localStorage.getItem("saveItem");
-    return simNotes ? JSON.parse(simNotes) : []
-}
-
-function writeNotes () {
-
-    const noteContainer = document.getElementById("main-container")
-
-    if(noteContainer.length === 0) {
-        noteContainer.innerHTML = ` <div class="kelas">
-            <h2>Add Notes</h2>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, earum!</h3>
-            <button onclick="addButton()">+</button>
-        </div>`
-        return
-    }
-    
-    noteContainer.innerHTML = notes.map(note =>`<h2>${note.keepJudul}</h2>
-            <h3>${note.keepContent}</h3>
-            <button onclick="addButton()">+</button>
-        </div>`).join('')
-
-
-}
-
-
-function generateId() {
-    return Date.now().toString()
-}
-
-function saveNote() {
-    localStorage.setItem("saveItem", JSON.stringify(notes));
-
-}
-
-function savedNotes () {
-    buttonSave.addEventListener("click", (e) => {
-        e.preventDefault();
-        keepNotes();
-       
-    })
-}
-
-
-
-function addButton() {
-    buttonAdd.addEventListener("click", function () {
-        cards.classList.toggle("show");
-        headerPage.classList.toggle("tampil")
-    });
-};
-function removeNotes() {
-    removeNote.addEventListener("click", () => {
-
-        cards.classList.remove("show")
-        headerPage.classList.remove("tampil")
-    })
-}
+    cards.classList.remove("show")
+    headerPage.classList.remove("tampil")
+})
 
 function closeOut() {
     document.addEventListener("click", (e) => {
@@ -104,12 +37,80 @@ function closeOut() {
     })
 }
 
+function keepNotes() {
+    event.preventDefault()
+
+
+    let keepJudul = inputJudul.value.trim();
+    let keepContent = inputContent.value.trim();
+    if (editNote) {
+        const keepNote = notes.findIndex(note => note.id == editNote)
+        notes[keepNote] = {
+            ...notes[keepNote],
+            keepJudul: keepJudul,
+            keepContent: keepContent,
+        }
+    } else {
+        notes.unshift({
+            id: Date.now().toString(),
+            Judul: keepJudul,
+            Content: keepContent
+        })
+    }
+
+    saveNote();
+}
+
+
+function loadingNotes() {
+    const simNotes = localStorage.getItem("saveItem");
+    return simNotes ? JSON.parse(simNotes) : []
+}
+
+function writeNotes() {
+
+
+    if (notes.length === 0) {
+        noteContainer.innerHTML = ` <div class="kelas">
+            <h2>Add Notes</h2>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, earum!</h3>
+            <button onclick="addButton()">+</button>
+        </div>`
+        return
+    } else {
+        noteContainer.innerHTML = notes.map(note => `
+         <div class="kelas-1">
+         <div class="kelas-2">
+        <h2>${note.Judul}</h2>
+        <div class="kelas-3">
+          <button onclick="addButton()">X</button>
+            <button onclick="addButton()"><i class ="fas fa-pen"></i></button>
+            </div>
+            </div>
+            <h3>${note.Content}</h3>
+
+        </div>
+        </div>`).join('');
+    }
+}
+
+
+function saveNote() {
+    localStorage.setItem("saveItem", JSON.stringify(notes));
+
+}
+
+function savedNotes() {
+    buttonSave.addEventListener("click", (e) => {
+        e.preventDefault();
+        keepNotes();
+
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    notes = loadingNotes();    
-    addButton();
-    removeNotes();
+    notes = loadingNotes();
     closeOut();
     writeNotes();
-
-   saveNote();
+    saveNote();
 })
